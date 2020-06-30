d53b58311c3edbe07f4b897d112939d5e93a4d49
Rafael <rafacardosodeandrade@gmail.com>
2020-06-30 13:24:47 -0500

    Machine Learning Model

--- Project/tutorial.py
+++ Project/tutorial.py
@@ -32,3 +32,22 @@ women = train_data.loc[train_data.Sex == 'female']['Survived']
 rate_women = sum(women)/len(women)
 
 print("% of women who survived", rate_women)
+
+#machine learning model
+
+from sklearn.ensemble import RandomForestClassifier
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X,y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived':predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
+
